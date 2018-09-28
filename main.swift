24cb48eeb251b605c2667f28ad1d90df6d804608
ciecmacs20171106578 <m15886077136@163.com>
2018-09-28 21:08:37 +0800

    Initial commit

---
+++ main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  test928_2
+//
+//  Created by 朝力萌 on 2018/9/28.
+//  Copyright © 2018年 朝力萌. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+print("Hello, 403!")
